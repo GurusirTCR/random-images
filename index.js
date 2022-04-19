684146c9c4df4980f79ec11d1f81907f07e7a360
Madhusudan Rawat <54324391+GurusirTCR@users.noreply.github.com>
2022-04-19 10:04:39 +0530

    Update new Code

--- index.js
+++ index.js
@@ -1,66 +1,81 @@
-$(document).ready(function () {
-  $(".apply").click(function () {
-    // var demo = $("#image-show").width();
-    // +"px";
-    var newWidth = $(".width").val();
 
-    $(".show_images").width(newWidth);
-    // demo = $("#image-show").width();
-    +"px";
-    var photos = $("#photos").val();
+var oldW = document.querySelector(".width").value;
+var oldP = document.querySelector(".photos").value;
+console.log(oldW,oldP);
+  $(document).ready(function () {
+    $(".apply").click(function () {
+      // var demo = $("#image-show").width();
+      // +"px";
+      var newWidth = $(".width").val();
+      
+      $(".show_images").width(newWidth);
+      demo = $("#image-show").width();
 
-    if (newWidth <= 50 || newWidth >= 1000 || photos < 1) {
-      alert("Not Valid input");
-      location.reload();
-    } else {
-      $.ajax({
-        url: "https://randomuser.me/api/?results=" + photos,
-        dataType: "json",
-        success: function (data) {
-          console.log(data.results[0]);
-          var newimg1 = newWidth / 50;
-          for (var i = 0; i < newimg1; i++) {
-            if (newWidth == photos * 50) {
-              var data1 = data.results[i];
-              var img = data1.picture.medium;
-              console.log(img);
-              $("#image-show").append(
-                `<img class="user-img" src="${img}" alt="">`
-              );
-              console.log("Width is LOOOO 111");
-            }
-            else if(newWidth > photos * 50){
+      +"px";
+      var photos = $("#photos").val();
+
+      const screenWidth = screen.width-20; 
+      console.log(screenWidth)
+      if (newWidth <= 50 || newWidth >= screenWidth || photos < 1) {
+        alert("Not Valid input");
+        location.reload();
+      } else {
+        $.ajax({
+          url: "https://randomuser.me/api/?results=" + photos,
+          dataType: "json",
+          success: function (data) {
+            console.log(data.results[0]);
+            var newimg1 = newWidth / 50;
+            $(".show_images").html('');
+            for (var i = 0; i < newimg1; i++)
+            {
+              if (newWidth == photos * 50) {
                 var data1 = data.results[i];
                 var img = data1.picture.medium;
                 $("#image-show").append(
-                    `<img class="user-img" src="${img}" alt="">`
-                  );
-                  console.log("###3333");
-            } 
-            else if (photos * 50 != newWidth) {
-              for (var i = 0; i < newimg1 - 1; i++) {
-                if (newWidth != photos * 50) {
+                  `<img class="user-img" src="${img}" alt="">`
+                );
+              }
+              else if(newWidth > photos * 50){
                   var data1 = data.results[i];
                   var img = data1.picture.medium;
-                  console.log(img);
                   $("#image-show").append(
-                    `<img class="user-img" src="${img}" alt="">`
-                  );
+                      `<img class="user-img" src="${img}" alt="">`
+                    );
+              } 
+              else if (photos * 50 != newWidth || newWidth%50==0) {
+                for (var i = 0; i < newimg1 - 1; i++) {
+                  if (newWidth != photos * 50) {
+                    var data1 = data.results[i];
+                    var img = data1.picture.medium;
+                    console.log(img);
+                    $("#image-show").append(
+                      `<img class="user-img" src="${img}" alt="">`
+                    );
+                  }
+                }
+               if(newWidth%50 == 0){ 
+                    $("#image-show").append(
+                      `<button id="trigger" onclick="modal()" class="user-img">+${
+                        photos- newimg1+1
+                      }</button>`
+                    );
+               }else{
+              
+                $("#image-show").append(
+                  `<button id="trigger" onclick="modal()" class="user-img">+${
+                    Math.round(photos -newimg1+1)
+                  }</button>`);
+                
                 }
               }
-              $("#image-show").append(
-                `<button id="trigger" onclick="modal()" class="user-img">+${
-                  photos - newimg1
-                }</button>`
-              );
-              console.log("Width is LOOOO 2222");
             }
-          }
-        },
-      });
-    }
+          },
+        });
+      }
+    });
   });
-});
+  
 
 function modal() {
   const trigger = document.querySelector("#trigger");
