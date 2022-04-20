4f452683865fd7a031308878eea9ecc020e440fe
Madhusudan Rawat <54324391+GurusirTCR@users.noreply.github.com>
2022-04-20 09:49:30 +0530

    Update The Yellow Box

--- index.js
+++ index.js
@@ -1,83 +1,75 @@
+window.onload = function () {
+  var newWidth = $(".width").val();
+  $(".show_images").width(newWidth);
+};
+$(document).ready(function () {
+  $(".apply").click(function () {
+    // var demo = $("#image-show").width();
+    // +"px";
+    var newWidth = $(".width").val();
+    $(".show_images").width(newWidth);
+    demo = $("#image-show").width();
 
-var oldW = document.querySelector(".width").value;
-var oldP = document.querySelector(".photos").value;
-console.log(oldW,oldP);
-  $(document).ready(function () {
-    $(".apply").click(function () {
-      // var demo = $("#image-show").width();
-      // +"px";
-      var newWidth = $(".width").val();
-      
-      $(".show_images").width(newWidth);
-      demo = $("#image-show").width();
+    +"px";
+    var photos = $("#photos").val();
 
-      +"px";
-      var photos = $("#photos").val();
+    const screenWidth = screen.width - 20;
 
-      const screenWidth = screen.width-20; 
-      console.log(screenWidth)
-      if (newWidth <= 50 || newWidth >= screenWidth || photos < 1) {
-        alert("Not Valid input");
-        location.reload();
-      } else {
-        $.ajax({
-          url: "https://randomuser.me/api/?results=" + photos,
-          dataType: "json",
-          success: function (data) {
-            console.log(data.results[0]);
-            var newimg1 = newWidth / 50;
-            $(".show_images").html('');
-            for (var i = 0; i < newimg1; i++)
-            {
-              if (newWidth == photos * 50) {
-                var data1 = data.results[i];
-                var img = data1.picture.medium;
-                $("#image-show").append(
-                  `<img class="user-img" src="${img}" alt="">`
-                );
-              }
-              else if(newWidth > photos * 50){
+    if (newWidth <= 50 || newWidth >= screenWidth || photos < 1) {
+      alert("Not Valid input");
+      location.reload();
+    } else {
+      $.ajax({
+        url: "https://randomuser.me/api/?results=" + photos,
+        dataType: "json",
+        success: function (data) {
+          console.log(data.results[0]);
+          var newimg1 = newWidth / 50;
+          console.log(newimg1);
+          $(".show_images").html("");
+          for (var i = 0; i < photos; i++) {
+            var data1 = data.results[i];
+            var img = data1.picture.medium;
+            if (photos == newimg1) {
+              $("#image-show").append(
+                `<img class="user-img" src="${img}" alt="">`
+              );
+            } else {
+              const total = photos - newimg1;
+              if (newWidth % 50 != 0) {
+                for (var i = 0; i < newimg1 - 2; i++) {
                   var data1 = data.results[i];
                   var img = data1.picture.medium;
                   $("#image-show").append(
-                      `<img class="user-img" src="${img}" alt="">`
-                    );
-              } 
-              else if (photos * 50 != newWidth || newWidth%50==0) {
-                for (var i = 0; i < newimg1 - 1; i++) {
-                  if (newWidth != photos * 50) {
-                    var data1 = data.results[i];
-                    var img = data1.picture.medium;
-                    console.log(img);
-                    $("#image-show").append(
-                      `<img class="user-img" src="${img}" alt="">`
-                    );
-                  }
-                }
-               if(newWidth%50 == 0){ 
-                    $("#image-show").append(
-                      `<button id="trigger" onclick="modal()" class="user-img">+${
-                        photos- newimg1+1
-                      }</button>`
-                    );
-               }else{
-              
-                $("#image-show").append(
-                  `<button id="trigger" onclick="modal()" class="user-img">+${
-                    Math.round(photos -newimg1+1)
-                  }</button>`);
+                    `<img class="user-img" src="${img}" alt="">`
+                  );
                 
                 }
-              }
-            }
-          },
-        });
-      }
-    });
-  });
-  
 
-function modal() {
+                $("#image-show").append(
+                  `<button class="user-img"  id="trigger">+${parseInt(total + 2)}</button>`
+                );
+
+              } else {
+
+                
+                $("#image-show").append(
+                  `<img class="user-img" src="${img}" alt="">`
+                );
+                $("#image-show").append(
+                  `<button class="user-img"  id="trigger">+${parseInt(total + 1)}</button>`
+                  );
+                }
+                break;
+              }
+              
+            }
+          }
+      });
+    }
+  });
+});
+
   const trigger = document.querySelector("#trigger");
   const modalWrapper = document.querySelector(".modal_wrapper");
   const closeBtn = document.querySelector(".close");
@@ -89,4 +81,4 @@ function modal() {
   closeBtn.addEventListener("click", function () {
     modalWrapper.classList.remove("active");
   });
-}
+
